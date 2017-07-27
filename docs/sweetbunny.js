22680ac07917fc5108bb3f4f87015f1a48a8130d
Mandy Chen <42mandychen@users.noreply.github.com>
2017-07-27 16:45:49 -0700

    Clean up repetitive code

--- docs/sweetbunny.js
+++ docs/sweetbunny.js
@@ -14,6 +14,7 @@
 // Numbers
 const imgHeight = 70;
 const imgWidth = 70;
+const numOfBunnies = 5;
 const numOfSweets = 8;
 const numOfOthers = 5;
 const canvasWidth = 700;
@@ -26,6 +27,7 @@ var numS = 0;
 var background;
 var sweet;
 var bunny;
+var rowOfBunnies;
 var bunnies;
 
 // Arrays
@@ -47,14 +49,13 @@ function preload() {
   background = loadImage("data/background.png");
   sweet = loadImage("data/sweet.png");
   bunny = loadImage("data/bunny.png");
-  bunny0 = loadImage("data/bunny0.png");
-  bunny1 = loadImage("data/bunny1.png");
-  bunny2 = loadImage("data/bunny2.png");
-  bunny3 = loadImage("data/bunny3.png");
-  bunny4 = loadImage("data/bunny4.png");
-  bunnies = loadImage("data/bunnies.png");
+  rowOfBunnies = loadImage("data/bunnies.png");
   youWin = loadImage("data/youWin.png");
   gameOver = loadImage("data/gameOver.png");
+
+  for (let i = 0; i < numOfBunnies; i++) {
+    bunnies[i] = loadImage("data/bunny" + i + ".png"); //load the i-th bunny
+  }
 
   // set up sweets
   for (let i = 0; i < numOfSweets; i++) {
@@ -104,19 +105,11 @@ function draw() {
       drawGameOver();
       break;
     case 0:
-      drawGamePlay0();
-      break;
     case 1:
-      drawGamePlay1();
-      break;
     case 2:
-      drawGamePlay2();
-      break;
     case 3:
-      drawGamePlay3();
-      break;
     case 4:
-      drawGamePlay4();
+      drawGamePlay(stageNum);
       break;
     case 5:
       drawYouWin();
@@ -161,8 +154,8 @@ function mousePressed() {
 function drawWelcome() {
   image(sweet, 200, 50, 300, 100);
   image(bunny, 210, 130, 300, 100);
-  image(bunnies, 120, 400, 500, 100);
-  image(bunnies, 120, 300, 500, 100);
+  image(rowOfBunnies, 120, 400, 500, 100);
+  image(rowOfBunnies, 120, 300, 500, 100);
   textSize(30);
   fill(0);
   text("Press 'S' to start", 230, 270);
@@ -198,12 +191,12 @@ function drawInstruction() {
   fill(0);
   text("the happier he becomes", 150, 400);
 
-  image(bunny4, 150, 430, 80, 80);
+  image(bunny[numOfBunnies], 150, 430, 80, 80);
   textSize(30);
   fill(0);
   text("You win", 230, 500);
 
-  image(bunny0, 360, 430, 80, 80);
+  image(bunny[0], 360, 430, 80, 80);
   textSize(30);
   fill(0);
   text("You lose", 450, 500);
@@ -213,9 +206,10 @@ function drawInstruction() {
   text("Press 'S' to start!", 450, 50);
 }
 
-function drawGamePlay0() {
-  image(bunny0, mouseX - 35, mouseY - 35, 70, 70);
-  image(bunnies, 100, 10, 300, 60);
+function drawGamePlay(level) {
+  let currBunny = bunnies[level];
+  image(currBunny, mouseX - 35, mouseY - 35, 70, 70);
+  image(rowOfBunnies, 100, 10, 300, 60);
   fill(0);
   textSize(30);
   text("Bunny", 10, 25);
@@ -224,7 +218,6 @@ function drawGamePlay0() {
   text("State:", 10, 65);
   stroke(255, 0, 0);
   strokeWeight(4);
-  line(340, 76, 410, 76);
 
   for (let i = 0; i < numOfSweets; i = i + 1) {
     if (visibleSwts[i] === true) {
@@ -260,211 +253,17 @@ function drawGamePlay0() {
   }
 }
 
-function drawGamePlay1() {
-  image(bunny1, mouseX - 35, mouseY - 35, 70, 70);
-  image(bunnies, 100, 10, 300, 60);
-  fill(0);
-  textSize(30);
-  text("Bunny", 10, 25);
-  fill(0);
-  textSize(30);
-  text("State:", 10, 65);
-  stroke(255, 0, 0);
-  strokeWeight(4);
-  line(276, 76, 340, 76);
-
-  for (let i = 0; i < numOfSweets; i = i + 1) {
-    if (visibleSwts[i] === true) {
-      image(sweets[i], xSwts[i], ySwts[i], 70, 70); //4. display the i-th image
-      xSwts[i] = xSwts[i] + sSwts[i];
-      if ((xSwts[i] > width) || (xSwts[i] < 0)) {
-        xSwts[i] = (Math.random() * (2 * imgWidth + canvasWidth)) - canvasWidth;
-        ySwts[i] = (Math.random() * (canvasHeight - 76)) + 76;
-
-        if (Math.random() * 2 < 1) {
-          sSwts[i] = (Math.random() * (15 - 8)) + 8;
-        } else {
-          sSwts[i] = (Math.random() * (15 - 8)) - 15;
-        }
-      }
-    }
+function drawResult(result) {
+  if (result === 'win') {
+    image(youWin, 160, 100, 350, 200);
+  } else { //'lose'
+    image(gameOver, 160, 100, 350, 200);
   }
-  for (let i = 0; i < numOfOthers; i = i + 1) {
-    if (visibleOths[i] === true) {
-      image(otherthings[i], xOths[i], yOths[i], 70, 70); //4. display the i-th image
-      xOths[i] = xOths[i] + sOths[i];
-      if ((xOths[i] > width) || (xOths[i] < 0)) {
-        xOths[i] = (Math.random() * (2 * imgWidth + canvasWidth)) - canvasWidth;
-        yOths[i] = (Math.random() * (canvasHeight - 76)) + 76;
-
-        if (Math.random() * 2 < 1) {
-          sOths[i] = (Math.random() * (15 - 8)) + 8;
-        } else {
-          sOths[i] = (Math.random() * (15 - 8)) - 15;
-        }
-      }
-    }
-  }
-}
-
-function drawGamePlay2() {
-  image(bunny2, mouseX - 35, mouseY - 35, 70, 70);
-  image(bunnies, 100, 10, 300, 60);
-  fill(0);
-  textSize(30);
-  text("Bunny", 10, 25);
-  fill(0);
-  textSize(30);
-  text("State:", 10, 65);
-  stroke(255, 0, 0);
-  strokeWeight(4);
-  line(220, 76, 276, 76);
-
-  for (let i = 0; i < numOfSweets; i = i + 1) {
-    if (visibleSwts[i] === true) {
-      image(sweets[i], xSwts[i], ySwts[i], 70, 70); //4. display the i-th image
-      xSwts[i] = xSwts[i] + sSwts[i];
-      if ((xSwts[i] > width) || (xSwts[i] < 0)) {
-        xSwts[i] = (Math.random() * (2 * imgWidth + canvasWidth)) - canvasWidth;
-        ySwts[i] = (Math.random() * (canvasHeight - 76)) + 76;
-
-        if (Math.random() * 2 < 1) {
-          sSwts[i] = (Math.random() * (15 - 8)) + 8;
-        } else {
-          sSwts[i] = (Math.random() * (15 - 8)) - 15;
-        }
-      }
-    }
-  }
-  for (let i = 0; i < numOfOthers; i = i + 1) {
-    if (visibleOths[i] === true) {
-      image(otherthings[i], xOths[i], yOths[i], 70, 70); //4. display the i-th image
-      xOths[i] = xOths[i] + sOths[i];
-      if ((xOths[i] > width) || (xOths[i] < 0)) {
-        xOths[i] = (Math.random() * (2 * imgWidth + canvasWidth)) - canvasWidth;
-        yOths[i] = (Math.random() * (canvasHeight - 76)) + 76;
-
-        if (Math.random() * 2 < 1) {
-          sOths[i] = (Math.random() * (15 - 8)) + 8;
-        } else {
-          sOths[i] = (Math.random() * (15 - 8)) - 15;
-        }
-      }
-    }
-  }
-}
-
-function drawGamePlay3() {
-  image(bunny3, mouseX - 35, mouseY - 35, 70, 70);
-  image(bunnies, 100, 10, 300, 60);
-  fill(0);
-  textSize(30);
-  text("Bunny", 10, 25);
-  fill(0);
-  textSize(30);
-  text("State:", 10, 65);
-  stroke(255, 0, 0);
-  strokeWeight(4);
-  line(158, 76, 220, 76);
-
-  for (let i = 0; i < numOfSweets; i++) {
-    if (visibleSwts[i] === true) {
-      image(sweets[i], xSwts[i], ySwts[i], 70, 70); //4. display the i-th image
-      xSwts[i] = xSwts[i] + sSwts[i];
-      if ((xSwts[i] > width) || (xSwts[i] < 0)) {
-        xSwts[i] = (Math.random() * (2 * imgWidth + canvasWidth)) - canvasWidth;
-        ySwts[i] = (Math.random() * (canvasHeight - 76)) + 76;
-
-        if (Math.random() * 2 < 1) {
-          sSwts[i] = (Math.random() * (15 - 8)) + 8;
-        } else {
-          sSwts[i] = (Math.random() * (15 - 8)) - 15;
-        }
-      }
-    }
-  }
-  for (let i = 0; i < numOfOthers; i++) {
-    if (visibleOths[i] === true) {
-      image(otherthings[i], xOths[i], yOths[i], 70, 70); //4. display the i-th image
-      xOths[i] = xOths[i] + sOths[i];
-      if ((xOths[i] > width) || (xOths[i] < 0)) {
-        xOths[i] = (Math.random() * (2 * imgWidth + canvasWidth)) - canvasWidth;
-        yOths[i] = (Math.random() * (canvasHeight - 76)) + 76;
-
-        if (Math.random() * 2 < 1) {
-          sOths[i] = (Math.random() * (15 - 8)) + 8;
-        } else {
-          sOths[i] = (Math.random() * (15 - 8)) - 15;
-        }
-      }
-    }
-  }
-}
-
-function drawGamePlay4() {
-
-  image(bunny4, mouseX - 35, mouseY - 35, 70, 70);
-  image(bunnies, 100, 10, 300, 60);
-  fill(0);
-  textSize(30);
-  text("Bunny", 10, 25);
-  fill(0);
-  textSize(30);
-  text("State:", 10, 65);
-  stroke(255, 0, 0);
-  strokeWeight(4);
-  line(98, 76, 158, 76);
-
-  for (let i = 0; i < numOfSweets; i++) {
-    if (visibleSwts[i] === true) {
-      image(sweets[i], xSwts[i], ySwts[i], 70, 70); //4. display the i-th image
-      xSwts[i] = xSwts[i] + sSwts[i];
-      if ((xSwts[i] > width) || (xSwts[i] < 0)) {
-        xSwts[i] = (Math.random() * (2 * imgWidth + canvasWidth)) - canvasWidth;
-        ySwts[i] = (Math.random() * (canvasHeight - 76)) + 76;
-
-        if (Math.random() * 2 < 1) {
-          sSwts[i] = (Math.random() * (15 - 8)) + 8;
-        } else {
-          sSwts[i] = (Math.random() * (15 - 8)) - 15;
-        }
-      }
-    }
-  }
-  for (let i = 0; i < numOfOthers; i++) {
-    if (visibleOths[i] === true) {
-      image(otherthings[i], xOths[i], yOths[i], 70, 70); //4. display the i-th image
-      xOths[i] = xOths[i] + sOths[i];
-      if ((xOths[i] > width) || (xOths[i] < 0)) {
-        xOths[i] = (Math.random() * (2 * imgWidth + canvasWidth)) - canvasWidth;
-        yOths[i] = (Math.random() * (canvasHeight - 76)) + 76;
-
-        if (Math.random() * 2 < 1) {
-          sOths[i] = (Math.random() * (15 - 8)) + 8;
-        } else {
-          sOths[i] = (Math.random() * (15 - 8)) - 15;
-        }
-      }
-    }
-  }
-}
-
-function drawYouWin() {
-  image(youWin, 160, 100, 350, 200);
   fill(0);
   textSize(30);
   text("Press 'R' to restart", 200, 320);
   fill(0);
   textSize(30);
   text("Press 'E' to exit", 200, 350);
-}
 
-function drawGameOver() {
-  image(gameOver, 160, 100, 350, 200);
-  fill(0);
-  textSize(30);
-  text("Press 'R' to restart", 200, 320);
-  fill(0);
-  textSize(30);
-  text("Press 'E' to exit", 200, 350);
 }
